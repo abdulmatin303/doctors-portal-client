94d19becc944e28c2177f5416da14313adeed71a
amr303 <amr.abdulmatin@gmail.com>
2022-05-20 03:34:11 +0600

    setup login page, implement sign in with google

--- src/Pages/Appoinment/Service.js
+++ src/Pages/Appoinment/Service.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Service = ({ service, setTreatment }) => {
     const { name, slots } = service;
     return (
-        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-            <div class="card-body text-center">
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+            <div className="card-body text-center">
                 <h2 className="text-xl font-bold text-secondary">{name}</h2>
                 <p>{
                     slots.length > 0
@@ -12,13 +12,13 @@ const Service = ({ service, setTreatment }) => {
                         : <span className='text-red-500'>Try another date.</span>
                 }</p>
                 <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
-                <div class="card-actions justify-center">
+                <div className="card-actions justify-center">
 
                     <label
                         for="booking-modal"
                         disabled={slots.length === 0}
                         onClick={() => setTreatment(service)}
-                        class="btn btn-sm btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary">Book Appoinment</label>
+                        className="btn btn-sm btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary">Book Appoinment</label>
 
                 </div>
             </div>
